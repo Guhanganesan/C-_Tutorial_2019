df80e34e11545cf7a4784c0f2f21ca090e825af0
Guhan Ganesan <bharadhiguhan@ymail.com>
2019-07-13 09:27:55 +0530

    Create Constructor.cpp

---
+++ OOPs/Class&Objects/Constructor.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+using namespace std;
+
+class Vehicle
+{
+    public:
+    Vehicle()
+    {
+        cout << "Vehicle name is bus"<<endl;
+    }
+};
+
+int main()
+{
+    Vehicle obj;
+    
+    return 0;
+}
+/*
+Answer:-
+
+Vehicle name is bus
+
+*/
