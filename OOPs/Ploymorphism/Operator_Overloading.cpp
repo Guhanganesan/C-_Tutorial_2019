9ac6686654648504328af3fc93bb08dff685457e
Guhan Ganesan <bharadhiguhan@ymail.com>
2019-07-21 12:16:47 +0530

    Create Operator_Overloading.cpp

---
+++ OOPs/Ploymorphism/Operator_Overloading.cpp
@@ -0,0 +1,43 @@
+#include<iostream>
+using namespace std;
+
+class Marks
+{
+    int internal=10, external=20;
+    public:
+    Marks()
+    {
+        internal=0;
+        external=0;
+    }
+    Marks(int im, int em)
+    {
+        internal=im;
+        external=em;
+    }
+    void display()
+    {
+        cout << internal <<endl << external <<endl;
+    }
+    
+    Marks operator+(Marks m)
+    {
+        Marks temp;
+        temp.internal=internal+m.internal;
+        temp.external=external+m.external;
+        return temp;
+    }
+};
+
+int main()
+{
+    Marks m1(20,30), m2(20,40);
+    Marks m3 =m1+m2;
+    m3.display();
+    return 0;
+}
+
+/*
+40
+70
+*/
