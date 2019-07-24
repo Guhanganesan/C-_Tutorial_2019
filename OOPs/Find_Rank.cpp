b29a59710ff4934d2bc6906e7b97808b9a59ef96
Guhan Ganesan <bharadhiguhan@ymail.com>
2019-07-24 17:46:24 +0530

    Create Find_Rank.cpp

---
+++ OOPs/Find_Rank.cpp
@@ -0,0 +1,125 @@
+#include<iostream>
+using namespace std;
+class College
+{
+    public:
+    char name[30], id[10];
+    int marks;
+    public:
+    void getDetails();
+    void display();
+};
+
+void College::getDetails()
+{
+    cout<<"Enter Name"<<endl;
+    cin>>name;
+    cout<<"Enter Id"<<endl;
+    cin>>id;
+    cout<<"Enter Marks"<<endl;
+    cin>>marks;
+}
+
+void College::display()
+{
+ cout<<"Student Name: "<<name <<endl;
+ cout<<"Student Id: "<<id<<endl;
+ cout<<"Student Marks "<<marks<<endl;
+}
+
+
+int main()
+{
+    
+    College obj[5];
+    char ch; int i=0, objCount=0;
+   
+    while(ch!='n')
+    {
+        cout<<"Enter Student"<< (i+1)<<" Details:"<<endl;
+        obj[i].getDetails();
+        cout<<"Do you want to continue"<<endl;
+        cin>>ch;
+        i++;objCount++;
+    }
+   //Sorting
+   College temp;
+   
+   for(int s=0; s<objCount; s++)
+   {
+       for(int t=0; t<objCount; t++)
+       {
+           if(obj[s].marks>obj[t].marks)
+           {
+               temp=obj[t];
+               obj[t]=obj[s];
+               obj[s]=temp;
+           }
+       }
+       
+   }
+   
+    for(int j=0; j<objCount; j++)
+    {
+        cout<<"Student"<<(j+1)<<" Information is:"<<endl;
+        obj[j].display();
+    }
+   
+    
+    return 0;
+}
+
+/*
+Enter Student1 Details:
+Enter Name
+Guhan
+Enter Id
+A001
+Enter Marks
+55
+Do you want to continue
+y
+Enter Student2 Details:
+Enter Name
+Anbu
+Enter Id
+A001
+Enter Marks
+45
+Do you want to continue
+y
+Enter Student3 Details:
+Enter Name
+Sowmya
+Enter Id
+A003
+Enter Marks
+85
+Do you want to continue
+y
+Enter Student4 Details:
+Enter Name
+Raja
+Enter Id
+A004
+Enter Marks
+66
+Do you want to continue
+n
+Student1 Information is:
+Student Name: Sowmya
+Student Id: A003
+Student Marks 85
+Student2 Information is:
+Student Name: Raja
+Student Id: A004
+Student Marks 66
+Student3 Information is:
+Student Name: Guhan
+Student Id: A001
+Student Marks 55
+Student4 Information is:
+Student Name: Anbu
+Student Id: A001
+Student Marks 45
+*/
