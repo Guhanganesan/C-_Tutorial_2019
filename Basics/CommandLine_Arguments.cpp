f37866294a329293640cbc66987970cea3676fad
Guhan Ganesan <bharadhiguhan@ymail.com>
2019-07-31 14:26:22 +0530

    Create CommandLine_Arguments.cpp

---
+++ Basics/CommandLine_Arguments.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+int main(int argc, char** argv) 
+{ 
+    cout << "You have entered " << argc<<endl; 
+    cout << argv[0] << "\n"; 
+    cout << argv[1] << "\n"; 
+    cout << argv[2] << "\n"; 
+    return 0; 
+}
+
+/*
+You have entered 4
+test
+raja
+jegan
+*/
